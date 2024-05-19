18ff23bc617bf3eb0071a27b91e804fc6016217b
klavrenuk1993 <klavrenuk1993>
2024-05-19 18:28:09 +0500

    AppAside - init component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import AppAside from "./components/aside/AppAside";
+
+
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            <AppAside />
+
+            <main className={'app__main'}>
+
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/aside/AppAside.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppAside = () => {
+    return (
+        <aside className={'app-side'}>
+            AppAside
+        </aside>
+    )
+}
+
+export default AppAside
